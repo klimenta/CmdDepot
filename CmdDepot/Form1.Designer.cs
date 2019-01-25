df90cb9fd4429c5198d89ad52ca60391940a748a
Kliment Andreev <klimenta@iandreev.com>
2019-01-25 15:31:34 -0500

    Final v0.1

---
+++ CmdDepot/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CmdDepot
+{
+    partial class CmdDepot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panHolder = new System.Windows.Forms.Panel();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tbCmd = new System.Windows.Forms.TextBox();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.ttTooltips = new System.Windows.Forms.ToolTip(this.components);
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.btnStayOnTop = new System.Windows.Forms.Button();
+            this.panHolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panHolder
+            // 
+            this.panHolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panHolder.Controls.Add(this.tbCmd);
+            this.panHolder.Location = new System.Drawing.Point(0, 32);
+            this.panHolder.Name = "panHolder";
+            this.panHolder.Size = new System.Drawing.Size(404, 50);
+            this.panHolder.TabIndex = 0;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(64, 2);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(30, 30);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "N";
+            this.ttTooltips.SetToolTip(this.btnNew, "New Instance");
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.Location = new System.Drawing.Point(128, 2);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(30, 30);
+            this.btnMinimize.TabIndex = 4;
+            this.btnMinimize.Text = "-";
+            this.ttTooltips.SetToolTip(this.btnMinimize, "Minimize");
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(160, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 30);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "X";
+            this.ttTooltips.SetToolTip(this.btnClose, "Exit Application");
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tbCmd
+            // 
+            this.tbCmd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCmd.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCmd.Location = new System.Drawing.Point(0, 1);
+            this.tbCmd.Multiline = true;
+            this.tbCmd.Name = "tbCmd";
+            this.tbCmd.Size = new System.Drawing.Size(402, 44);
+            this.tbCmd.TabIndex = 2;
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaste.Location = new System.Drawing.Point(32, 2);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(30, 30);
+            this.btnPaste.TabIndex = 1;
+            this.btnPaste.Text = "P";
+            this.ttTooltips.SetToolTip(this.btnPaste, "Paste from Clipboard");
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(0, 2);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(30, 30);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "C";
+            this.ttTooltips.SetToolTip(this.btnCopy, "Copy to Clipboard");
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // ttTooltips
+            // 
+            this.ttTooltips.IsBalloon = true;
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.AutoSize = true;
+            this.lblAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAbout.Location = new System.Drawing.Point(196, 10);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(210, 20);
+            this.lblAbout.TabIndex = 6;
+            this.lblAbout.Text = "Kliment Andreev - 2019, v0.1";
+            // 
+            // btnStayOnTop
+            // 
+            this.btnStayOnTop.Location = new System.Drawing.Point(96, 2);
+            this.btnStayOnTop.Name = "btnStayOnTop";
+            this.btnStayOnTop.Size = new System.Drawing.Size(30, 30);
+            this.btnStayOnTop.TabIndex = 7;
+            this.btnStayOnTop.Text = "T";
+            this.ttTooltips.SetToolTip(this.btnStayOnTop, "Stay On Top");
+            this.btnStayOnTop.UseVisualStyleBackColor = true;
+            this.btnStayOnTop.Click += new System.EventHandler(this.btnStayOnTop_Click);
+            // 
+            // CmdDepot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 80);
+            this.Controls.Add(this.btnStayOnTop);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.panHolder);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCopy);
+            this.Name = "CmdDepot";
+            this.Text = "CmdDepot";
+            this.panHolder.ResumeLayout(false);
+            this.panHolder.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panHolder;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox tbCmd;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ToolTip ttTooltips;
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.Button btnStayOnTop;
+    }
+}
+
---
+++ CmdDepot/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CmdDepot
+{
+    public partial class CmdDepot : Form
+    {
+        public CmdDepot()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+        private const int cGrip = 16;      // Grip size
+        private const int cCaption = 5;   // Caption bar height;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(Brushes.Chartreuse, rc);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {  // Trap WM_NCHITTEST
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;  // HTCAPTION
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17; // HTBOTTOMRIGHT
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+    
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            tbCmd.Paste();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbCmd.Text);
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+        }
+
+        private void btnStayOnTop_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+            if (this.TopMost) {
+                btnStayOnTop.BackColor = Color.Tomato;
+            } else
+            {
+                btnStayOnTop.BackColor = btnCopy.BackColor;
+            }            
+        }
+    }
+}
